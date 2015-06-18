c944123735521fa66a6e3f40a9d890c89c25a5df
nvkzNemo <nvkz.nemo@gmail.com>
2015-06-18 17:52:36 +0700

    Fit on Screen zoom

--- filesToArtboards.jsx
+++ filesToArtboards.jsx
@@ -1,29 +1,29 @@
 function cTID(s) {return app.charIDToTypeID(s);}
 function sTID(s) {return app.stringIDToTypeID(s);}
 
-function newArtboard(_name,_w,_h) {
+function newArtboard(_name, _w, _h) {
     var desc6 = new ActionDescriptor();
-        var ref1 = new ActionReference();
-        ref1.putClass( sTID('artboardSection') );
-    desc6.putReference( cTID('null'), ref1 );
-        var desc7 = new ActionDescriptor();
-        desc7.putString( cTID('Nm  '), _name );
-    desc6.putObject( cTID('Usng'), sTID('artboardSection'), desc7 );
-        var desc8 = new ActionDescriptor();
-        desc8.putDouble( cTID('Top '), 0.000000 );
-        desc8.putDouble( cTID('Left'), 0.000000 );
-        desc8.putDouble( cTID('Btom'), _h );
-        desc8.putDouble( cTID('Rght'), _w );
-    desc6.putObject( sTID('artboardRect'), sTID('classFloatRect'), desc8 );
-    executeAction( cTID('Mk  '), desc6, DialogModes.NO );
+    var ref1 = new ActionReference();
+    ref1.putClass(sTID('artboardSection'));
+    desc6.putReference(cTID('null'), ref1);
+    var desc7 = new ActionDescriptor();
+    desc7.putString(cTID('Nm  '), _name);
+    desc6.putObject(cTID('Usng'), sTID('artboardSection'), desc7);
+    var desc8 = new ActionDescriptor();
+    desc8.putDouble(cTID('Top '), 0.000000);
+    desc8.putDouble(cTID('Left'), 0.000000);
+    desc8.putDouble(cTID('Btom'), _h);
+    desc8.putDouble(cTID('Rght'), _w);
+    desc6.putObject(sTID('artboardRect'), sTID('classFloatRect'), desc8);
+    executeAction(cTID('Mk  '), desc6, DialogModes.NO);
 }
 
 function selectAllLayers() {
     var desc23 = new ActionDescriptor();
-        var ref5 = new ActionReference();
-        ref5.putEnumerated( cTID('Lyr '), cTID('Ordn'), cTID('Trgt') );
-    desc23.putReference( cTID('null'), ref5 );
-    executeAction( sTID('selectAllLayers'), desc23, DialogModes.NO );
+    var ref5 = new ActionReference();
+    ref5.putEnumerated(cTID('Lyr '), cTID('Ordn'), cTID('Trgt'));
+    desc23.putReference(cTID('null'), ref5);
+    executeAction(sTID('selectAllLayers'), desc23, DialogModes.NO);
 }
 
 function main() {
@@ -35,8 +35,8 @@ function main() {
         for (var i = 0; i < fileList.length; i++) {
             app.open(fileList[i]);
             currentDocWidth = app.activeDocument.width.value + 20;
-		    selectAllLayers();
-            newArtboard(app.activeDocument.name,app.activeDocument.width.value, app.activeDocument.height.value);
+            selectAllLayers();
+            newArtboard(app.activeDocument.name, app.activeDocument.width.value, app.activeDocument.height.value);
             app.activeDocument.activeLayer.duplicate(doc, ElementPlacement.INSIDE);
             app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
             if (i > 0) {
@@ -45,7 +45,8 @@ function main() {
             delta = delta + currentDocWidth;
         }
     }
-    doc.crop([0,0,app.activeDocument.width,app.activeDocument.height],0,delta);
+    doc.crop([0, 0, app.activeDocument.width, app.activeDocument.height], 0, delta);
+    app.runMenuItem(charIDToTypeID(("FtOn")));
     alert('Done!');
 }
 
